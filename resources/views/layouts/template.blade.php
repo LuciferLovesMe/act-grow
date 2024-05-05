65552334be9ac2b0857cdbf56e2ea78fe8f1fe4f
LuciferLovesMe <abhiemsmile@gmail.com>
2024-05-05 22:35:59 +0700

    feat/navbar: fix navbar

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -43,7 +43,7 @@
   @include('sweetalert::alert')
 
   <!-- ======= Top Bar ======= -->
-  <section id="topbar" class="d-flex align-items-center">
+  {{-- <section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
       <div class="contact-info d-flex align-items-center">
         <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
@@ -56,45 +56,39 @@
         <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
       </div>
     </div>
-  </section>
+  </section> --}}
 
   <!-- ======= Header ======= -->
   <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="index.html">BizLand<span>.</span></a></h1>
+      <h1 class="logo"><img src="{{asset('assets/img/logo_actgrow.png')}}" alt=""></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
+          @if (auth()->check())
+              @if (auth()->user()->role == 'Lembaga')
+                  <li><a class="nav-link scrollto active" href="">Lembaga Sertifikasi</a></li>
+                  <li><a class="nav-link scrollto" href="">Kontak</a></li>
+              @else
+                <li class="dropdown"><a href="#"><span>Layanan</span> <i class="bi bi-chevron-down"></i></a>
+                  <ul>
+                    <li><a href="#"><span><i class="fa fa-user"></i></span>Profil Lembaga Sertifikasi</a></li>
+                    <li><a href="#"><span><i class="fa fa-file"></i></span>Sertifikasi</a></li>
+                  </ul>
+                </li>
+              @endif
+          @else
+          @endif
+          <li><a class="nav-link scrollto" href="">Artikel</a></li>
       </nav><!-- .navbar -->
 
     </div>
   </header><!-- End Header -->
 
-  <!-- ======= Hero Section ======= -->
+  {{-- <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
     <div class="container" data-aos="zoom-out" data-aos-delay="100">
       <h1>Welcome to <span>BizLand</span></h1>
@@ -103,7 +97,7 @@
         <a href="#about" class="btn-get-started scrollto">Get Started</a>
       </div>
     </div>
-  </section><!-- End Hero -->
+  </section><!-- End Hero --> --}}
 
   <main id="main">
     {{-- Content --}}
