8620da1717bb4bbb005b8a18c1a7635712c53db4
LuciferLovesMe <abhiemsmile@gmail.com>
2024-05-08 00:27:39 +0700

    feat/auth: fix login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,51 @@
-@extends('layouts.app')
+@extends('layouts.template')
+
+<style>
+    body,html{
+    height:100%;
+}
+</style>
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+    <div class="inner-page">
+        <div class="container h-100">
+            <div class="row h-100 justify-content-center align-items-center">
+                <div class="col-md-8" style="background-image: url('{{ asset('assets/img/background_login.png') }}'); background-repeat: no-repeat, no-repeat;">
+                    <h3><b>Masuk atau Daftar</b></h3>
+                    <p><b>Dapatkan informasi terbaru seputar pasar Indonesia, standar internasional, dan transaksi pribadi dalam satu akun. Pantau kebutuhan anda dengan lebih mudah bersama ACTGROW</b></p>
+                </div>
+                <div class="col-md-4">
+                    <form action="{{ route('login') }}" method="post" enctype="multipart/form-data">
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <input type="text" placeholder="Username" class="form-control" required autocomplete="username" name="username">
+                        <div class="input-group mt-2">
+                            <input type="password" name="password" class="form-control " placeholder="Password" id="password" autocomplete="new-password" required>
+                            <button class="btn btn-outline-secondary btn-password" type="button" id=""><i class="fa fa-eye"></i></button>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+    
+                        <div class="d-flex justify-content-around mt-2">
+                            <button class="btn btn-success" type="submit">Login</button>
+                            <a href="{{ route('register') }}"><button class="btn btn-outline-secondary">Daftar</button></a>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
+
+@push('custom-script')
+    <script>
+        $( '.btn-password').on('click', function() {
+            if($(this).children('i').hasClass('fa-eye-slash')) {
+                $(this).children('i').removeClass('fa-eye-slash')
+                $(this).children('i').addClass('fa-eye')
+                $(this).parent().children('input').attr('type', 'password')
+            } else {
+                $(this).children('i').removeClass('fa-eye')
+                $(this).children('i').addClass('fa-eye-slash')
+                $(this).parent().children('input').attr('type', 'text')
+            }
+        })
+    </script>
+@endpush
